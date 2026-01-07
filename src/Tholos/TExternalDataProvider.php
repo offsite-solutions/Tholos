3faf4538c2176b79fce5f35345884e654da2b46b
Laszlo Banfalvi <laszlo.banfalvi@offsite-solutions.com>
2026-01-07 17:11:01 +0100

    - TExternalDataProvider safe array fix

--- src/Tholos/TExternalDataProvider.php
+++ src/Tholos/TExternalDataProvider.php
@@ -286,6 +286,7 @@
           
           $this->setProperty('Opened', 'true');
           $this->setProperty('Result', $resultParameters[$this->getProperty('ResultParameter', 'result')], 'ARRAY');
+          Tholos::$logger->debug(print_r($this->getProperty('Result'),true), $this);
           $this->setProperty('ResultType', 'ARRAY');
           $this->setProperty('RowCount', count($resultParameters[$this->getProperty('ResultParameter', 'result')]));
           $this->setProperty('TotalRowCount', Eisodos::$utils->safe_array_value($resultParameters, $this->getProperty('TotalRowCountField'), $this->getProperty('RowCount')));
